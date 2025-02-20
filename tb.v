58610343051a87b9503dcc020539c13d7267e32b
Vasu <105120013+CodeitVasu@users.noreply.github.com>
2025-02-20 19:38:31 +0530

    Update tb.v

--- tb.v
+++ tb.v
@@ -3,13 +3,14 @@
 
 module time_tb();
     reg clk_125MHz,rst,start,stop;
-    wire [7:0] count;
-    wire [1:0] min_counter;
+    wire [5:0] count;
+    wire [1:0] min;
     wire [26:0] counter;
     wire clk_1Hz;
     wire done;
     
-    time_counter t0(clk_125MHz,rst,start,stop,min_counter,clk_1Hz,done,count,counter);
+    //time_counter t0(clk_125MHz,rst,start,stop,min_counter,clk_1Hz,done,count,counter);
+    clock_Expt t0(clk_125MHz,clk_1Hz,min,done,rst, start, stop,counter,count);
      begin
         always begin
         #4; // 125 MHz clock period = 8 ns (half period = 4 ns)
@@ -32,7 +33,12 @@ module time_tb();
         stop = 0;
 
         // Simulate for a long time to observe the 1 Hz clock
-        #20000000; // Simulate for 1 second (1,000,000,000 ns)
+        #200000000; // Simulate for 1 second (1,000,000,000 ns)
+        
+        
+        rst = 1;
+        
+        #4000000000;
         
        
 
